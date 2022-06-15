9fdff0ab5909e92b3184c023e28029c99ab7f25e
518H0090 <nhoxhieuro5@gmail.com>
2022-06-15 15:49:30 +0700

    Basic Form

--- Frontend/reactnewsblog/src/App.js
+++ Frontend/reactnewsblog/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- Frontend/reactnewsblog/src/components/Nav.jsx
+++ Frontend/reactnewsblog/src/components/Nav.jsx
@@ -1,102 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <>
-      <nav className="navbar bg-light fixed-top">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Offcanvas navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="offcanvas offcanvas-end"
-            tabindex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                Offcanvas
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Link
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="offcanvasNavbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="offcanvasNavbarDropdown"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <form class="d-flex" role="search">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button class="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand ms-md-5 bg-info text-white p-2 border border-2 border-info rounded-pill ">
+          TrungHieuIT1204
+        </span>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse me-md-5 d-md-flex justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <div class="navbar-nav">
+            <span class="nav-link active" aria-current="page">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                Home
+              </Link>
+            </span>
+            <span class="nav-link text-decoration-none">
+              <Link to="/login" style={{ textDecoration: "none" }}>
+                Login
+              </Link>
+            </span>
+            <span class="nav-link text-decoration-none">
+              <Link to="/register" style={{ textDecoration: "none" }}>
+                Register
+              </Link>
+            </span>
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
